1742389702c0dc13df607c408f92f6d1bd75d176
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2024-12-14 16:56:52 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nodemonkes.4everland.store',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   assetPrefix: '/nodemonkes-gif/',
 }
