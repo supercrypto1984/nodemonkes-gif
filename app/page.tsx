aeb2f580082f0aa042ae4eac65cf92d255dcf70d
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2024-12-14 18:20:27 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import GifGenerator from '../components/GifGenerator'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 py-8 px-4">
-      <div className="container mx-auto">
-        <GifGenerator />
-      </div>
+    <main className="min-h-screen">
+      <GifGenerator />
     </main>
   )
 }
+
