6c8640d37bf0860748f1e9cadf948f7516aa82ef
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2025-09-27 20:44:23 +0800

    Create gif.d.ts

---
+++ types/gif.d.ts
@@ -0,0 +1,39 @@
+declare module "gif.js" {
+  export interface GIFOptions {
+    workers?: number
+    quality?: number
+    width?: number
+    height?: number
+    workerScript?: string
+    dither?: boolean | string
+    debug?: boolean
+    repeat?: number
+    background?: string
+    transparent?: string | null
+    globalPalette?: boolean | number[]
+  }
+
+  export default class GIF {
+    constructor(options: GIFOptions)
+
+    addFrame(
+      imageElement: CanvasImageSource,
+      options?: {
+        delay?: number
+        copy?: boolean
+        dispose?: number
+      },
+    ): void
+
+    on(event: "finished", callback: (blob: Blob) => void): void
+    on(event: "progress", callback: (percent: number) => void): void
+    on(event: "abort", callback: () => void): void
+
+    render(): void
+    abort(): void
+
+    running: boolean
+    options: GIFOptions
+    frames: any[]
+  }
+}
