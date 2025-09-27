77b0a147380038fd8a4ee881a0ef3ab53de289e1
supercrypto1984 <85349327+supercrypto1984@users.noreply.github.com>
2025-09-27 20:44:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: '/nodemonkes-gif',
-  assetPrefix: '/nodemonkes-gif/',
+  basePath: "/nodemonkes-gif",
+  assetPrefix: "/nodemonkes-gif/",
+  distDir: "out",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
